7ed26c1038adabf173c356887d980920a65b8315
AlejandroMar <motungo@hotmail.com>
2018-05-29 13:35:40 +0200

    add notes

--- notes.js
+++ notes.js
@@ -1,7 +1,23 @@
 console.log('starting note.js');
+const fs = require('fs');
+
 
 const addNote = (title, body) => {
-    console.log('Adding note', title, body);  
+    let notes = []
+    const note = {
+        title: title,
+        body: body
+    };
+    if (!fs.existsSync('./notes-data.json')){
+        fs.writeFileSync('notes-data.json', '[]');
+    } 
+    const notesString = fs.readFileSync('notes-data.json');
+    notes = JSON.parse(notesString)
+    notes.push(note)
+    const noteToString = JSON.stringify(notes);
+    fs.writeFileSync('notes-data.json', noteToString);
+    console.log('Item added: ', `title: ${title}`, `body: ${body}`);
+    
 };
 
 const getAll = () => {
@@ -10,12 +26,12 @@ const getAll = () => {
 
 const getNote = (title, body) => {
     console.log('reading node: ', title, body);
-    
+
 };
 
 const removeNote = (title, body) => {
     console.log('removing note: ', title);
-};
+}
 
 
 //it's very important to export the functions and functionality that I need in other modules
--- playground/json.js
+++ playground/json.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 
+
 const originalNote = {
     title:'Some Title',
     body: 'Some body'
